b1491e8067ef279d81363b70b16b190a915e3105
Nick Perrine <157837775+nsp08980@users.noreply.github.com>
2024-10-09 13:50:39 -0500

    Implemented Non-Functional Animation Changes
    Implemented code to save space for movement animation and implemented code for animating casting spells

--- Spellcraft/Assets/Scripts/PlayerMovement.cs
+++ Spellcraft/Assets/Scripts/PlayerMovement.cs
@@ -22,40 +22,35 @@ public class PlayerMovement : MonoBehaviour
         if(Input.GetAxisRaw("Vertical") > 0) {
             anim.SetBool("Down", false);
             anim.SetBool("Up", true);
-            anim.SetBool("Right", false);
-            anim.SetBool("Left", false);
+            anim.SetBool("Side", false);
             sr.flipX = false;
         }
         // Animates the character moving down
         else if(Input.GetAxisRaw("Vertical") < 0) {
             anim.SetBool("Down", true);
             anim.SetBool("Up", false);
-            anim.SetBool("Right", false);
-            anim.SetBool("Left", false);
+            anim.SetBool("Side", false);
             sr.flipX = false;
         }
         // Animates the character moving right
         else if(Input.GetAxisRaw("Horizontal") > 0) {
             anim.SetBool("Down", false);
             anim.SetBool("Up", false);
-            anim.SetBool("Right", true);
-            anim.SetBool("Left", false);
+            anim.SetBool("Side", true);
             sr.flipX = true;
         }
         // Animates the character moving left
         else if(Input.GetAxisRaw("Horizontal") < 0) {
             anim.SetBool("Down", false);
             anim.SetBool("Up", false);
-            anim.SetBool("Right", false);
-            anim.SetBool("Left", true);
+            anim.SetBool("Side", true);
             sr.flipX = false;
         }
         // Cancels movement animations so that we don't have movement when we aren't moving
         else {
             anim.SetBool("Down", false);
             anim.SetBool("Up", false);
-            anim.SetBool("Right", false);
-            anim.SetBool("Left", false);
+            anim.SetBool("Side", false);
         }
     }
 }
--- Spellcraft/Assets/Scripts/SpellsBase.cs
+++ Spellcraft/Assets/Scripts/SpellsBase.cs
@@ -9,12 +9,19 @@ public class SpellsBase : MonoBehaviour
     public Image SpellInfo;
     public GameObject path;
     public GameObject spell;
+    Animator anim;
+    SpriteRenderer player;
+
     public Sprite trajectory;
     public Sprite empty;
+    Vector3 mousePosition;
+    float angle;
     void Start()
     {
         SpellInfo.gameObject.SetActive(false);
         path.GetComponent<SpriteRenderer>().sprite = empty;
+        anim = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -31,8 +38,7 @@ public class SpellsBase : MonoBehaviour
         }
         // Spawns a base spell projectile when the 'E' key is pressed
         if(Input.GetKeyUp(KeyCode.E)) {
-            GameObject tempSpell = Instantiate(spell, path.transform.position, path.transform.rotation);
-            tempSpell.transform.right = path.transform.right.normalized;
+            StartCoroutine("castSpell");
         }
     }
 
@@ -54,15 +60,42 @@ public class SpellsBase : MonoBehaviour
     void prepareSpell()
     {
         // Takes in the mouse's current position
-        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Input.mousePosition;
         // Translates the mouse's current position into something applicable to the game's window
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         // Sets x and y of the mouse's position to the distance between it and the trajectory's current position
         mousePosition.x = mousePosition.x - path.transform.position.x;
         mousePosition.y = mousePosition.y - path.transform.position.y;
         // Calculates the angle of the where the mouse is in relation to where the trajectory line is (minus 90 degrees for some reason)
-        float angle = (Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg) - 90f;
+        angle = (Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg) - 90f;
         // Sets the trajectory line's rotation to be on the mouse's position and since we can't snap the mouse, it will look smooth
         path.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+
+    IEnumerator castSpell()
+    {
+        GameObject tempSpell = Instantiate(spell, path.transform.position, path.transform.rotation);
+        tempSpell.transform.right = path.transform.right.normalized;
+        if((angle > 0 && angle < 45) || (angle < 360 && angle > 315)) {
+            anim.SetBool("CastSide", true);
+            player.flipX = false;
+        }
+        else if((angle >= 45 || angle <= 135)) {
+            anim.SetBool("CastLeft", true);
+            player.flipX = false;
+        }
+        else if((angle > 135 || angle < 225)) {
+            anim.SetBool("CastDown", true);
+            player.flipX = false;
+        }
+        else {
+            anim.SetBool("CastSide", true);
+            player.flipX = true;
+        }
+        yield return new WaitForSeconds(1.5f);
+        anim.SetBool("CastSide", false);
+        anim.SetBool("CaseUp", false);
+        anim.SetBool("CastDown", false);
+        player.flipX = false;
     }
 }
