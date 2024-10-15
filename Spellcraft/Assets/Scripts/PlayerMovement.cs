a7a94cd8f92d9a40801526dfe45168e031a27eae
Samuel Brodersen <samuel.brodersen49@gmail.com>
2024-10-15 11:36:41 -0500

    fully integrated movement with animations and reworked if else(s) to overide

--- Spellcraft/Assets/Scripts/PlayerMovement.cs
+++ Spellcraft/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     Animator anim;
     SpriteRenderer sr;
     public float velocity = 10f;
+    public float speed = 5f;
     float moveHorizontal, moveVertical;
     void Start()
     {
@@ -20,34 +21,46 @@ public class PlayerMovement : MonoBehaviour
     {
         // Animates the character moving up
         if(Input.GetAxisRaw("Vertical") > 0) {
+            Vector3 temp = this.transform.position; ;
+            temp.y += speed * Time.deltaTime;
+            this.transform.position = temp;
             anim.SetBool("Down", false);
             anim.SetBool("Up", true);
             anim.SetBool("Side", false);
             sr.flipX = false;
         }
         // Animates the character moving down
-        else if(Input.GetAxisRaw("Vertical") < 0) {
+        if(Input.GetAxisRaw("Vertical") < 0) {
+            Vector3 temp = this.transform.position; ;
+            temp.y -= speed * Time.deltaTime;
+            this.transform.position = temp;
             anim.SetBool("Down", true);
             anim.SetBool("Up", false);
             anim.SetBool("Side", false);
             sr.flipX = false;
         }
         // Animates the character moving right
-        else if(Input.GetAxisRaw("Horizontal") > 0) {
+        if(Input.GetAxisRaw("Horizontal") > 0) {
+            Vector3 temp = this.transform.position; ;
+            temp.x += speed * Time.deltaTime;
+            this.transform.position = temp;
             anim.SetBool("Down", false);
             anim.SetBool("Up", false);
             anim.SetBool("Side", true);
             sr.flipX = true;
         }
         // Animates the character moving left
-        else if(Input.GetAxisRaw("Horizontal") < 0) {
+        if(Input.GetAxisRaw("Horizontal") < 0) {
+            Vector3 temp = this.transform.position; ;
+            temp.x -= speed * Time.deltaTime;
+            this.transform.position = temp;
             anim.SetBool("Down", false);
             anim.SetBool("Up", false);
             anim.SetBool("Side", true);
             sr.flipX = false;
         }
         // Cancels movement animations so that we don't have movement when we aren't moving
-        else {
+        if (Input.GetAxisRaw("Vertical") == 0 && Input.GetAxisRaw("Horizontal") == 0) {
             anim.SetBool("Down", false);
             anim.SetBool("Up", false);
             anim.SetBool("Side", false);
