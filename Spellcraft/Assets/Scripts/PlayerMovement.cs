c80d28ec7c514ffc33acf9de7476c4df565475f4
Zach-ar-y <ZDM69360@ucmo.edu>
2024-10-18 20:26:38 -0500

    edited world 1
    added collectibles and an end goal, added new tags
    player script actually moves character around

--- Spellcraft/Assets/Scripts/PlayerMovement.cs
+++ Spellcraft/Assets/Scripts/PlayerMovement.cs
@@ -4,53 +4,73 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody2D rb;
-    Animator anim;
-    SpriteRenderer sr;
-    public float velocity = 10f;
-    float moveHorizontal, moveVertical;
+    public float moveSpeed = 5f; // Movement speed
+    private Animator animator; // Reference to Animator component
+
+    private Vector2 movement; // To store player movement input
+
+    // Initialize components in Start
     void Start()
     {
-        rb = this.GetComponent<Rigidbody2D>();
-        anim = this.GetComponent<Animator>();
-        sr = this.GetComponent<SpriteRenderer>();
+        // Automatically find the Animator component
+        animator = GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.LogError("Animator component missing from this GameObject!");
+        }
     }
 
     void Update()
     {
-        // Animates the character moving up
-        if(Input.GetAxisRaw("Vertical") > 0) {
-            anim.SetBool("Down", false);
-            anim.SetBool("Up", true);
-            anim.SetBool("Side", false);
-            sr.flipX = false;
+        // Get player input from WASD/Arrow keys
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // Normalize movement to avoid faster diagonal movement
+        if (movement.magnitude > 1)
+        {
+            movement.Normalize();
         }
-        // Animates the character moving down
-        else if(Input.GetAxisRaw("Vertical") < 0) {
-            anim.SetBool("Down", true);
-            anim.SetBool("Up", false);
-            anim.SetBool("Side", false);
-            sr.flipX = false;
+
+        // Move the character by changing the Transform's position
+        Vector3 newPosition = transform.position + new Vector3(movement.x, movement.y, 0) * moveSpeed * Time.deltaTime;
+        transform.position = newPosition;
+
+
+        // Check movement direction and set animation parameters
+        if (movement.y > 0)
+        {
+            // Moving up
+            animator.SetBool("Up", true);
+            animator.SetBool("Down", false);
+            animator.SetBool("Side", false);
         }
-        // Animates the character moving right
-        else if(Input.GetAxisRaw("Horizontal") > 0) {
-            anim.SetBool("Down", false);
-            anim.SetBool("Up", false);
-            anim.SetBool("Side", true);
-            sr.flipX = true;
+        else if (movement.y < 0)
+        {
+            // Moving down
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", true);
+            animator.SetBool("Side", false);
         }
-        // Animates the character moving left
-        else if(Input.GetAxisRaw("Horizontal") < 0) {
-            anim.SetBool("Down", false);
-            anim.SetBool("Up", false);
-            anim.SetBool("Side", true);
-            sr.flipX = false;
+        else if (movement.x != 0)
+        {
+            // Moving sideways
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
+            animator.SetBool("Side", true);
+
+            // Flip character based on movement direction
+            if (movement.x > 0)
+                transform.localScale = new Vector3(-2, 2, 2); // Facing right
+            else if (movement.x < 0)
+                transform.localScale = new Vector3(2, 2, 2); // Facing left
         }
-        // Cancels movement animations so that we don't have movement when we aren't moving
-        else {
-            anim.SetBool("Down", false);
-            anim.SetBool("Up", false);
-            anim.SetBool("Side", false);
+        else
+        {
+            // If no movement, stop all animations
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
+            animator.SetBool("Side", false);
         }
     }
 }
